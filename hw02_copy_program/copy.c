7efadd59c7efe15b83674b747775553bbcb17f14
bmg23 <bmg23@gold17.cs.calvin.edu>
2020-02-11 15:20:12 -0500

    hw02 initial

---
+++ hw02_copy_program/copy.c
@@ -0,0 +1,19 @@
+/* Homework 02: Using system calls from a C program
+ * 
+ * Goal: 
+ *  Write a program in C that copies 
+ *  the contents of one file to 
+ *  another. 
+ * 
+ * Author: Brian Goins (bmg23) 
+ * Date: 2/11/2020
+ */  
+
+#include <stdio.h>     // printf()
+
+int main (int argc, char *argv[]) { 
+    printf("hey"); 
+
+
+    printf("\n"); 
+}
