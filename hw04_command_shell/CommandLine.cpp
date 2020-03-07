5cea2c92efadcfa8c84b7f10689adf424c4128a5
chv5 <chv5@gold05.cs.calvin.edu>
2020-03-07 17:56:31 -0500

    Adds constructor that takes in istream as well as getCommand

--- hw04_command_shell/CommandLine.cpp
+++ hw04_command_shell/CommandLine.cpp
@@ -11,6 +11,7 @@ using namespace std;
 #include <iostream>
 #include <vector>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include <bits/stdc++.h>
 
@@ -20,40 +21,61 @@ class CommandLine {
     char* argv[];
 
     public:
+
+        //CommandLine constructor takes in iostream input and parses into argv while incrementing argc
         CommandLine(istream& in);
         char* getCommand() const;
 
 
 
-} cmdl;
+};
 
-//CommandLine constructor takes in iostream input and parses into argv while incrementing argc
 CommandLine::CommandLine(istream& in){
-    
-    //Read in the command from istream
-    string command;
-    in >> command;
+            //Read in the command from istream
+            char *command;
+            in >> command;
 
-    //Initialize variables for command tokenization
-    stringstream strm(command);
-    string interm;
-    int i = 0;
-    //Tokenize input command into argument vector and increment argument counter
-    while(getline(strm, interm, ' ')){
-        *CommandLine::argv[i].push_back(interm);
-        argc++;    
-    }
+            //state keeps track of whether or not the loop is tracking a word
+            int state = 0;
+            int size = 0;
+            //count both characters and words in the command
+            while (*command){
+                
+                //If the next character is a separator, set the state to 0
+                if (*command == ' ' || *command == '\n' || *command == '\t'){
+                    state = 0;
+                }
 
-    //Allocate memory to hold command 
-    calloc(argc, command.length());
+                //If state is 0, set state = 1 and increment argument counter
+                else if (state == 0){
+                    state = 1;
+                    argc ++;
+                }
+                ++size;
+                ++command;
+            }
 
+            //Allocate space for argv based on size of the command and number of arguments
+            char* argv = (char*)calloc(argc, size);
+
+            //Initialize variables for command tokenization
+            int i;
+            
+            //Tokenize input command into argument vector
+            char *token = strtok(command, " ");
+            while (token != NULL){
+                *token >> argv[i];
+                token = strtok(NULL, " "); 
+            }
+                       
 }
 
-char* getCommand() const { 
+char* CommandLine::getCommand() const { 
     return argv[0];
     }
 
 int main (){
     cout << "Enter command: ";
     CommandLine cmdl (cin);
+    
 }
